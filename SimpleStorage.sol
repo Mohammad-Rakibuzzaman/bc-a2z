3b8aa9702beac59809f6c4f3503bc06dbca292c5
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-01-15 05:01:03 +0600

    first commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,4 @@
+pragma solidity 0.8.7; //0.8.20
+
+
+
---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+// >=0.8.7 < 0.9.0;
+// ^0.8.7 == means greater or this specific version
+//0.8.20
+pragma solidity ^0.8.7;
+
+//this is most likely as class like oop based languages. ofcourse 
+/*
+    hypothetically it can be depoloy and run as
+    a workable contract. so this is kinda
+    valid contract.
+    congo writing valid contract...
+*/
+contract SimpleStorage {
+
+
+
+}

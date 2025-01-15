7ccae93618991b720f55e56d22c991071b4cf8e2
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-01-16 00:04:00 +0600

    2:35 till score; next array&structs

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -18,12 +18,40 @@ pragma solidity ^0.8.7;
 contract SimpleStorage {
     //primitive datatype uint256, int256, bool, string, address, bytes32
     
+    //here public is kind of retrieve function which returns some uint value 
     uint256 public favoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+        // uint256 testVar = 5;
+        // retrieve();
+
     }
 
+    // view and pure keyword used in solidity to notate a function thats doesnt actually have to spend gas to run.
+
+    //here public which returns some uint value 
+    //below is the resemble function of upper ( uint256 public favoriteNumber; ) declaration
+
+    // we cant update data using view function
+   
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+    
+    //pure also disallow update favorite number also cant read favorite number either
+    //pure function something like below
+    // function add() public pure returns(uint256) {
+    //     return (1+1);
+    // }
+
+
+    /*
+    // scope
+    function something() public {
+        testVar = 6;
+    }
+    */
 
 
 }
